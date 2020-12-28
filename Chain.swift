767822e243d3b82d94d016a7c953573e08a0e36c
Muukii <muukii.app@gmail.com>
2020-12-28 16:46:00 +0900

    Init

---
+++ Chain.swift
@@ -0,0 +1,57 @@
+public protocol _Chain_OptionalProtocol {
+  associatedtype Wrapped
+  var _chain_wrapped: Wrapped? { get }
+}
+
+extension Optional: _Chain_OptionalProtocol {
+  public var _chain_wrapped: Wrapped? {
+    self
+  }
+}
+
+postfix operator ^
+
+postfix func ^ <T>(argument: T) -> Chain<T> {
+  .init(argument)
+}
+
+postfix func ^ <T>(argument: Chain<T>) -> T {
+  argument.value
+}
+
+public struct Chain<Value> {
+
+  public let value: Value
+
+  public init(_ value: Value) {
+    self.value = value
+  }
+
+  public func map<U>(_ transform: (Value) throws -> U) rethrows -> Chain<U> {
+    .init(try transform(value))
+  }
+
+  public func flatMap<U>(_ transform: (Value) throws -> Chain<U>) rethrows -> Chain<U> {
+    .init(try transform(value).value)
+  }
+
+  public func `do`(_ applier: (Value) throws -> Void) rethrows -> Chain<Value> {
+    try applier(value)
+    return self
+  }
+
+  public func filter(_ filter: (Value) -> Bool) -> Chain<Value?> {
+    guard filter(value) else {
+      return .init(nil)
+    }
+    return .init(value)
+  }
+
+  public func ifEmpty(_ fallbackValue: Value.Wrapped) -> Chain<Value.Wrapped> where Value : _Chain_OptionalProtocol {
+    if let wrapped = self.value._chain_wrapped {
+      return .init(wrapped)
+    }
+    return .init(fallbackValue)
+  }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Chain
+
+An experimental project of creating method chain API using something like Monad.
+
+## Usage
+
+```swift
+
+let result: String = ""^.map { Int($0) }.do { print($0 as Any) }^
+
+let value: String = ""^.filter { !$0.isEmpty }.ifEmpty("b")^
+
+let view: UIView = UIView()^.do {
+  $0.backgroundColor = .white
+}^
+```
