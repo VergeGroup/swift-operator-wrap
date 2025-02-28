1c93de710c5944bd942af5ec99a1e23e1f1774ce
Muukii <muukii.app@gmail.com>
2025-02-28 12:04:34 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
   name: "Wrap",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6)
+  ],
   products: [
     .library(name: "Wrap", targets: ["Wrap"]),
   ],
